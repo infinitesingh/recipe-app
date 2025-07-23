05489254e4662c8daa44752c1f6bcbb11894bb61
Anant Vikram Singh <anantvikramsingh@Anants-MacBook-Pro.local>
2025-07-23 19:10:58 +0530

    admin config

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
 # Register your models here.
+
+# admin.site.register(models.User)
+
+
+""" Customize the admin page"""
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
